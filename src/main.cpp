b621991af9cb69f992c1f00918d64f4a6106fd9f
lion_techn <teles_tech@icloud.com>
2024-01-08 23:13:02 +0900

    add lib : FastLED

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include <FastLED.h>
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(2,INPUT);
+  Serial.begin(115200);
+  pinMode(25,OUTPUT);
+  pinMode(26,OUTPUT);
+  pinMode(33,OUTPUT);
+  ledcAttachPin(25,0);
+  ledcAttachPin(26,1);
+  ledcAttachPin(33,2);
+  ledcSetup(0,1000,10);
+  ledcSetup(1,1000,10);
+  ledcSetup(2,1000,10);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println(analogRead(2));
+  ledcWrite(0,0);
+  ledcWrite(1,0);
+  ledcWrite(2,0);
+  delay(1000);
+  ledcWrite(0,100);
+  ledcWrite(1,100);
+  ledcWrite(2,100);
+  delay(1000);
 }
