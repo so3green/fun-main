6a9de8513d7b7d4779251a1b4752ed5cfa7e27a5
lion_techn <teles_tech@icloud.com>
2024-01-10 20:52:49 +0900

    change

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include <FastLED.h>
 void setup() {
-  pinMode(2,INPUT);
-  Serial.begin(115200);
   pinMode(25,OUTPUT);
   pinMode(26,OUTPUT);
   pinMode(33,OUTPUT);
@@ -12,16 +10,11 @@ void setup() {
   ledcSetup(0,1000,10);
   ledcSetup(1,1000,10);
   ledcSetup(2,1000,10);
-}
-
-void loop() {
-  Serial.println(analogRead(2));
-  ledcWrite(0,0);
-  ledcWrite(1,0);
-  ledcWrite(2,0);
-  delay(1000);
   ledcWrite(0,100);
   ledcWrite(1,100);
   ledcWrite(2,100);
-  delay(1000);
+}
+
+void loop() {
+
 }
